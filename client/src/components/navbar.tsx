7f72c01be571f581bf5de15179743314437de28e
Jesus <jesus.munoz01@utrgv.edu>
2024-03-08 20:35:02 -0600

    added a test navbar

---
+++ client/src/components/navbar.tsx
@@ -0,0 +1,14 @@
+
+const Navbar = () => 
+{
+    return (
+        <nav>
+        <ul>
+            <li>Home</li>
+            <li>Login</li>
+            <li>Test</li>
+        </ul>
+        </nav>
+    );
+};
+export default Navbar;
