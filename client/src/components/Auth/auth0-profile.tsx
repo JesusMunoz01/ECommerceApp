eb67fa2d033b446fcda16667f18fda5fba7fd533
Jesus <jesus.munoz01@utrgv.edu>
2024-03-11 22:31:46 -0500

    updated test route

--- client/src/components/Auth/auth0-profile.tsx
+++ client/src/components/Auth/auth0-profile.tsx
@@ -1,33 +1,9 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect, useState } from "react";
 
-const Test = () => {
+const Account = () => {
     const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
-    const [userMetadata, setUserMetadata] = useState(null);
-
-    useEffect(() => {
-        const getUserMetadata = async () => {
-      
-          try {
-            const accessToken = await getAccessTokenSilently();
-            const userDetailsByIdUrl = `${import.meta.env.VITE_API_URL}`;
-      
-            const metadataResponse = await fetch(userDetailsByIdUrl, {
-              headers: {
-                authorization: `Bearer ${accessToken}`,
-              },
-            });
-      
-            const responseData = await metadataResponse.json();
-      
-            console.log(responseData);
-          } catch (e: any) {
-            console.log(e.message);
-          }
-        };
-      
-        getUserMetadata();
-      }, [getAccessTokenSilently, user?.sub]);
+    //const [userMetadata, setUserMetadata] = useState(null);
 
     const sendRequest = async () => {
         try {
@@ -49,24 +25,29 @@ const Test = () => {
 
     return (
       <div>
-        {isAuthenticated && (
+        {isAuthenticated ? 
           <div>
             <img src={user?.picture} alt={user?.name} />
             <h2>{user?.name}</h2>
             <p>{user?.email}</p>
             <h3>User Metadata</h3>
-            {userMetadata ? (
+            {/* {userMetadata ? (
               <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
               ) : (
                 "No user metadata defined"
-                )}
+                )} */}
             <br></br>
           </div>
-        )}
+         : 
+          <div>
+            <h2>Not Logged In</h2>
+            <p>Please log in to see your account details</p>
+          </div>
+        }
         <button onClick={sendRequest}>Send Request</button>
       </div>
       );
 };
 
 
-export default Test;
+export default Account;
