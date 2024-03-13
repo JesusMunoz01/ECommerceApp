ca1386cc96b2c134d455f9f65d65828fcf65b243
Jesus <jesus.munoz01@utrgv.edu>
2024-03-12 20:37:36 -0500

    moved create logic to users endpoint

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -12,32 +12,8 @@ export class AppController {
   }
 
   @UseGuards(AuthGuard('jwt'))
-  @Post("/user")
+  @Post()
   async getHello2(@Body() body: { data: string; test: string }): Promise<{ data: string; }> {
-    const formData = new URLSearchParams();
-      formData.append('grant_type', 'client_credentials');
-      formData.append('client_id', process.env.API_CLIENT_ID);
-      formData.append('client_secret', process.env.API_CLIENT_SECRET);
-      formData.append('audience', process.env.AUTH0_MANAGEMENT_AUDIENCE);
-    const getAPIToken = await fetch(`${process.env.AUTH0_ISSUER_URL}oauth/token`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: formData
-    });
-    const aToken = await getAPIToken.json();
-    const userResponse = await fetch(`${process.env.AUTH0_MANAGEMENT_AUDIENCE}users/${body.data}`, {
-      headers: {
-        authorization: `Bearer ${aToken.access_token}`,
-      }
-    });
-    const userData = await userResponse.json();
-    if(userData.logins_count === 1) {
-      console.log("First login")
-      return { data: "First login" };
-    }
-    console.log("Post Request Done")
     return this.appService.getHello();
   }
 }
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard("jwt"))
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Post("/create")
+    async createUser(@Body() body: { data: string; test: string }): Promise<{ message: string; }> {
+          const formData = new URLSearchParams();
+            formData.append('grant_type', 'client_credentials');
+            formData.append('client_id', process.env.API_CLIENT_ID);
+            formData.append('client_secret', process.env.API_CLIENT_SECRET);
+            formData.append('audience', process.env.AUTH0_MANAGEMENT_AUDIENCE);
+          const getAPIToken = await fetch(`${process.env.AUTH0_ISSUER_URL}oauth/token`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: formData
+          });
+          const aToken = await getAPIToken.json();
+          const userResponse = await fetch(`${process.env.AUTH0_MANAGEMENT_AUDIENCE}users/${body.data}`, {
+            headers: {
+              authorization: `Bearer ${aToken.access_token}`,
+            }
+          });
+          const userData = await userResponse.json();
+          if(userData.logins_count === 1) {
+            console.log("First login")
+            return { message: "First login" };
+          }
+          console.log("Post Request Done")
+          return this.usersService.createUser();
+        }
+        
+}
