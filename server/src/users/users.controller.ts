835e8ea8ba5fabe45f4391bef3f9382d408b1b63
Jesus <jesus.munoz01@utrgv.edu>
2024-03-14 21:23:25 -0500

    added a basic route for getting the userid

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthGuard } from '@nestjs/passport';
 
@@ -6,6 +6,11 @@ import { AuthGuard } from '@nestjs/passport';
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
+
+    @Get(":id")
+    async getUser(@Param("id") id: string): Promise<{ message: string; }> {
+      return this.usersService.getUser(id);
+    }
 
     @Post("/create")
     async createUser(@Body() body: { data: string; test: string }): Promise<{ message: string; }> {
