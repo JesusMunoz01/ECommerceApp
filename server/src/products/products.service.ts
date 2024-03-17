3cc98740269c14205acb225cd3478893ffc36c90
Jesus <jesus.munoz01@utrgv.edu>
2024-03-16 20:08:41 -0500

    added a base function for getting products

--- server/src/products/products.service.ts
+++ server/src/products/products.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { AppService } from 'src/app.service';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    constructor(private appService: AppService) {}
+    private connection = this.appService.connection;
+
+    async getProducts(): Promise<{ message: string; }> {
+        try{
+            this.connection.query(`SELECT * FROM products`, (err, results) => {
+                if(err) {
+                    console.log(err);
+                    return { message: "Error getting products" };
+                }
+                console.log(results);
+                return { message: "Products retrieved successfully" };
+            });
+        }
+        catch(err) {
+            console.log(err);
+            return { message: "Error getting products" };
+        }
+    }
+}
