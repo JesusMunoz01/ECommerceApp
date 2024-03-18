115a22c5bd3d5bdd91a8fb67a1194be26cbf3f0a
Jesus <jesus.munoz01@utrgv.edu>
2024-03-17 19:57:46 -0500

    added dto and basic imports for orders

--- server/src/orders/orders.controller.ts
+++ server/src/orders/orders.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { OrdersService } from './orders.service';
 
+@UseGuards(AuthGuard("jwt"))
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+    constructor(private readonly ordersService: OrdersService) {}
+}
--- server/src/orders/orders.service.ts
+++ server/src/orders/orders.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { AppService } from 'src/app.service';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+    constructor(private appService: AppService) {}
+    private connection = this.appService.connection;
+}
