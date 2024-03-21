461f5fd789cdc822c84c00bc8e351c8b733dd719
Jesus <jesus.munoz01@utrgv.edu>
2024-03-20 21:43:25 -0500

    added stripe basic controller

--- server/src/payment/payment.controller.ts
+++ server/src/payment/payment.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { StripeService } from './payment.service';
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller('payment')
-export class PaymentController {}
+@UseGuards(AuthGuard('jwt'))
+@Controller('payments')
+export class PaymentController {
+  constructor(private readonly stripeService: StripeService) {}
+
+  @Post('create-payment-intent')
+  async createPaymentIntent(@Body() body: { amount: number }): Promise<{ clientSecret: string }> {
+    const clientSecret = await this.stripeService.createPaymentIntent(body.amount);
+    return { clientSecret };
+  }
+
+  // Controllers
+}
