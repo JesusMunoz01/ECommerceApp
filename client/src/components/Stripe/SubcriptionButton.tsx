5e733c3931bec82cc719341be22aeebd022e5486
Jesus <jesus.munoz01@utrgv.edu>
2024-03-25 23:22:43 -0500

    added subscription component

---
+++ client/src/components/Stripe/SubcriptionButton.tsx
@@ -0,0 +1,49 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { useMutation } from "@tanstack/react-query";
+
+
+const SubscriptionButton = () => {
+    const { getAccessTokenSilently, user } = useAuth0();
+
+    const subscribeQuery = useMutation({
+        mutationKey: ['subscription'],
+        mutationFn: async () => {
+
+            let userId = null;
+            if(user)
+                userId = user.sub;
+
+            const token = await getAccessTokenSilently();
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/payments/subscriptions`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    userId: userId,
+                }),
+            });
+            const data = await response.json();
+            window.location = data.url;
+        },
+    })
+
+
+    if (subscribeQuery.isPending) {
+        return <div>Loading...</div>;
+    }
+
+    if (subscribeQuery.isError) {
+        return <div>Error: </div>;
+    }
+
+    return (
+        <div>
+            <button className="mb-4 disabled:bg-gray-600 disabled:cursor-not-allowed disabled:border-none" 
+                onClick={() => subscribeQuery.mutate()} disabled={subscribeQuery.isPending}>Subscribe</button>
+        </div>
+    );
+};
+
+export default SubscriptionButton;
