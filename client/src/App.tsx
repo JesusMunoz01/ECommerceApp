c676b59dac949ef52e7b0085c5617165e144ed1d
Jesus <jesus.munoz01@utrgv.edu>
2024-03-27 21:58:30 -0500

    added css for first upgrade item

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import HomePage from './pages/home.tsx'
 import Account from './components/Auth/auth0-profile'
 import Cart, { CartItem } from './pages/cart.tsx'
 import { useState } from 'react'
+import UpgradePage from './pages/upgrade.tsx'
 
 function App() {
   const [cart, setCart] = useState<CartItem[]>([])
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<HomePage setCart={setCart}/>} />
           <Route path="/account" element={<Account />} />
           <Route path="/cart" element={<Cart cart={cart} setCart={setCart}/>} />
+          <Route path='/upgrade' element={<UpgradePage/>} />
           <Route path="*" element={<h1>Not Found</h1>} />
         </Routes>
       </Router>
--- client/src/pages/upgrade.tsx
+++ client/src/pages/upgrade.tsx
@@ -2,41 +2,43 @@ import SubscriptionButton from "../components/Stripe/SubcriptionButton";
 
 const UpgradePage = () => {
     return (
-        <div>
+        <div className="flex items-center flex-col">
         <h1>Upgrade your Account</h1>
         <p>Upgrade your account to access premium features</p>
-        <div className="mb-4">
-            <h2>Current</h2>
-            <p>Free</p>
-            <div>
-                <p>Features</p>
-                <ul>
-                    <li>Feature 1</li>
-                    <li>Feature 2</li>
-                    <li>Feature 3</li>
-                </ul>
+        <div className="flex gap-5">
+            <div className="mb-4 flex items-center flex-col bg-slate-500 rounded-md w-80">
+                <h2 className="text-4xl">Current</h2>
+                <h3>Free</h3>
+                <div className="flex items-center flex-col">
+                    <p className="border-b-2">Features</p>
+                    <ul className="flex items-center flex-col">
+                        <li>Buy Items</li>
+                        <li>Sell Items {`(50 Max)`}</li>
+                        <li>Feature 3</li>
+                    </ul>
+                </div>
             </div>
-        </div>
-        <div className="mb-4">
-            <h2>Premium</h2>
-            <div>
-                <p>Features</p>
-                <ul>
-                    <li>Feature 1</li>
-                    <li>Feature 2</li>
-                    <li>Feature 3</li>
-                </ul>
+            <div className="mb-4">
+                <h2 className="text-4xl">Premium</h2>
+                <div>
+                    <p>Features</p>
+                    <ul>
+                        <li>Feature 1</li>
+                        <li>Feature 2</li>
+                        <li>Feature 3</li>
+                    </ul>
+                </div>
             </div>
-        </div>
-        <div className="mb-4">
-            <h2>Enterprise</h2>
-            <div>
-                <p>Features</p>
-                <ul>
-                    <li>Feature 1</li>
-                    <li>Feature 2</li>
-                    <li>Feature 3</li>
-                </ul>
+            <div className="mb-4">
+                <h2 className="text-4xl">Enterprise</h2>
+                <div>
+                    <p>Features</p>
+                    <ul>
+                        <li>Feature 1</li>
+                        <li>Feature 2</li>
+                        <li>Feature 3</li>
+                    </ul>
+                </div>
             </div>
         </div>
         <SubscriptionButton />
