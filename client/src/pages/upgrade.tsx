16f2f0379825a5ef7d64ca6382cbc2b88aaf238a
Jesus <jesus.munoz01@utrgv.edu>
2024-03-27 22:34:14 -0500

    updated page css to be full height minus navbar

--- client/src/pages/upgrade.tsx
+++ client/src/pages/upgrade.tsx
@@ -2,12 +2,12 @@ import SubscriptionButton from "../components/Stripe/SubcriptionButton";
 
 const UpgradePage = () => {
     return (
-        <div className="flex items-center flex-col">
-        <h1>Upgrade your Account</h1>
+        <div className="flex items-center flex-col" style={{height: "calc(100vh - 6rem)"}}>
+        <h1 className="h-20">Upgrade your Account</h1>
         <p>Upgrade your account to access premium features</p>
         <div className="flex gap-5">
             <div className="mb-4 flex items-center flex-col bg-slate-500 rounded-md w-80">
-                <h2 className="text-4xl">Current</h2>
+                <h2 className="text-4xl h-12">Current</h2>
                 <h3>Free</h3>
                 <div className="flex items-center flex-col">
                     <p className="border-b-2">Features</p>
@@ -20,7 +20,7 @@ const UpgradePage = () => {
                 </div>
             </div>
             <div className="mb-4 flex items-center flex-col bg-slate-500 rounded-md w-80">
-                <h2 className="text-4xl">Premium</h2>
+                <h2 className="text-4xl h-12">Premium</h2>
                 <h3>$1.99</h3>
                 <div className="flex items-center flex-col">
                     <p className="border-b-2">Features</p>
@@ -32,7 +32,7 @@ const UpgradePage = () => {
                 </div>
             </div>
             <div className="mb-4 flex items-center flex-col bg-slate-500 rounded-md w-80">
-                <h2 className="text-4xl">Enterprise</h2>
+                <h2 className="text-4xl h-12">Enterprise</h2>
                 <h3>$2.99</h3>
                 <div className="flex items-center flex-col">
                     <p className="border-b-2">Features</p>
