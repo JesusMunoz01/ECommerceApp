4878246cd91951d70467d06361c5dcce757620c2
Jesus <jesus.munoz01@utrgv.edu>
2024-03-27 22:01:54 -0500

    updated css for remaining items

--- client/src/pages/upgrade.tsx
+++ client/src/pages/upgrade.tsx
@@ -14,29 +14,29 @@ const UpgradePage = () => {
                     <ul className="flex items-center flex-col">
                         <li>Buy Items</li>
                         <li>Sell Items {`(50 Max)`}</li>
-                        <li>Feature 3</li>
                     </ul>
                 </div>
             </div>
-            <div className="mb-4">
+            <div className="mb-4 flex items-center flex-col bg-slate-500 rounded-md w-80">
                 <h2 className="text-4xl">Premium</h2>
-                <div>
-                    <p>Features</p>
-                    <ul>
-                        <li>Feature 1</li>
-                        <li>Feature 2</li>
-                        <li>Feature 3</li>
+                <h3>$1.99</h3>
+                <div className="flex items-center flex-col">
+                    <p className="border-b-2">Features</p>
+                    <ul className="flex items-center flex-col">
+                        <li>Buy Items</li>
+                        <li>Sell Items {`(1000 Max)`}</li>
                     </ul>
                 </div>
             </div>
-            <div className="mb-4">
+            <div className="mb-4 flex items-center flex-col bg-slate-500 rounded-md w-80">
                 <h2 className="text-4xl">Enterprise</h2>
-                <div>
-                    <p>Features</p>
-                    <ul>
-                        <li>Feature 1</li>
-                        <li>Feature 2</li>
-                        <li>Feature 3</li>
+                <h3>$2.99</h3>
+                <div className="flex items-center flex-col">
+                    <p className="border-b-2">Features</p>
+                    <ul className="flex items-center flex-col">
+                        <li>Buy Items</li>
+                        <li>Sell Items {`(Infinite)`}</li>
+                        <li>Create a Brand Page</li>
                     </ul>
                 </div>
             </div>
