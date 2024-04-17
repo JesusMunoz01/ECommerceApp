f8ea2f282fce0aaea086788df87bd293b605a8c8
Jesus <jesus.munoz01@utrgv.edu>
2024-04-16 21:52:19 -0500

    updated sidebar css and added temp text without links

--- client/src/components/Sidebar/sidebar.tsx
+++ client/src/components/Sidebar/sidebar.tsx
@@ -1,3 +1,4 @@
+import LogoutButton from "../Auth/auth0-logout";
 
 type SidebarProps = {
     isOpen: boolean;
@@ -7,9 +8,18 @@ type SidebarProps = {
 const Sidebar = ({ isOpen, toggle }: SidebarProps) => {
     return (
       isOpen && (
-        <div className="sidebar">
-          <h2>Sidebar</h2>
-          <button onClick={toggle}>Close Sidebar</button>
+        <div className="flex flex-col items-center bg-slate-700 h-full w-32 justify-between">
+          <div className="mt-2">
+            <h2 className="text-xl border-b border-slate-500 w-full">Account</h2>
+            <ul className="flex flex-col mt-4 gap-4">
+              <li>Profile</li>
+              <li>Settings</li>
+            </ul>
+          </div>
+          <div className="mb-2 flex items-center flex-col gap-2 border-t border-slate-500 w-fit pt-2">
+            <LogoutButton />
+            <button onClick={toggle}>Close Sidebar</button>
+          </div>
         </div>
       )
     );
