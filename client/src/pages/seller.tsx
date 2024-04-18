5c737dfc4d21b0c264c65b8f1123663ab9d6ac66
Jesus <jesus.munoz01@utrgv.edu>
2024-04-17 21:05:30 -0500

    added user product list to account page, added sell page link to sidebar and sidebar shows in sell page

--- client/src/pages/seller.tsx
+++ client/src/pages/seller.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import ProductForm from "../components/Products/productForm";
+import Sidebar from "../components/Sidebar/sidebar";
 
 const SellPage = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+      setIsSidebarOpen(!isSidebarOpen);
+    };
 
     return (
-        <div>
+        <div className="flex gap-2 w-full" style={{ height: 'calc(100vh - 6rem)' }}>
+        <div className="flex flex-col h-full">
+          {!isSidebarOpen && <button onClick={toggleSidebar}>Toggle Sidebar</button>}
+            <Sidebar isOpen={isSidebarOpen} toggle={toggleSidebar} />
+        </div>
             <h1>Sell Page</h1>
             <p>Create A Product to Sell</p>
             <ProductForm />
