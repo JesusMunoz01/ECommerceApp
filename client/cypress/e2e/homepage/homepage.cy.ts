7060d2471a95319d4711cb2a27a4f6e8bc38b119
Jesus <jesus.munoz01@utrgv.edu>
2024-05-05 19:07:33 -0500

    renamed file and added tests for navbar

--- client/cypress/e2e/homepage/homepage.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('Homepage', () => {
-  it('should display the homepage', () => {
-    cy.visit('http://localhost:5173/');
-    cy.get('.text-green-500').should('have.text', 'Home');
-  });
-});
