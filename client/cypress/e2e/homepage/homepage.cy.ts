e1a87c5bade3cd916f027f64085ad077be0417f2
Jesus <jesus.munoz01@utrgv.edu>
2024-05-07 20:47:30 -0500

    added a template test for multiple items

--- client/cypress/e2e/homepage/homepage.cy.ts
+++ client/cypress/e2e/homepage/homepage.cy.ts
@@ -19,4 +19,14 @@ describe('Homepage', () => {
         cy.get('h1').should('have.text', 'Cart');
         cy.get('button').contains('Remove').should('exist');
     });
+
+    it("should select multiple products with multiple quantities and add them to cart", () => {
+        cy.visit('http://localhost:5173/');
+        cy.get('button').contains('Add to cart').click();
+        cy.get('button').contains('Add to cart').click();
+        cy.get('a').contains('Cart').click();
+        cy.url().should('include', '/cart');
+        cy.get('h1').should('have.text', 'Cart');
+        cy.get('button').contains('Remove').should('exist');
+    });
 });
