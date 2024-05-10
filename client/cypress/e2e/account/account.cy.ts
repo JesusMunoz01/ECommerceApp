4f778434f513fb8933b74a5b79feacba94cb712e
Jesus <jesus.munoz01@utrgv.edu>
2024-05-09 20:38:27 -0500

    added visibility tests for text

--- client/cypress/e2e/account/account.cy.ts
+++ client/cypress/e2e/account/account.cy.ts
@@ -6,4 +6,20 @@ describe('Account Tests', () => {
     it("shouldnt display account page if not logged in", () => {
         cy.contains('Not Found').should('be.visible');
     });
+
+    it("should display account page if logged in", () => {
+        cy.login();
+        cy.visit('http://localhost:5173/account');
+        cy.contains('Account Details').should('be.visible');
+        cy.contains('Your Products').should('be.visible');
+        cy.contains('Your Brand').should('be.visible');
+    });
+
+    it("should check the sidebar items", () => {
+        cy.login();
+        cy.visit('http://localhost:5173/account');
+        cy.get('button').contains('Profile').should('be.visible');
+        cy.get('button').contains('Products').should('be.visible');
+        cy.get('button').contains('Brands').should('be.visible');
+    });
 });
