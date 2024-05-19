72f1fdbbf6b71512f2c1708bb155496d462f528b
Jesus <jesus.munoz01@utrgv.edu>
2024-05-18 19:18:19 -0500

    connect brand page to backend

--- client/src/pages/brands.tsx
+++ client/src/pages/brands.tsx
@@ -1,8 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
 
 const BrandsPage = () => {
+    const brandsQuery = useQuery({
+        queryKey: ['brands'],
+        queryFn: async () => {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/brands`);
+            const data = await response.json();
+            return data;
+        }
+    });
+
+    if(brandsQuery.isLoading) return <div>Loading...</div>;
+    if(brandsQuery.isError) return <div>Error fetching brands</div>;
+
     return (
-        <div>
-        <h1>Brands</h1>
+        <div className="m-1">
+            <h1>Brands</h1>
+            <div className="grid grid-cols-5 gap-4 w-12/12 m-2">
+                {brandsQuery.data.data.map((brand: any) => (
+                    <div key={brand.id} className="border-2 p-2 bg-slate-700">
+                        <h2>{brand.name}</h2>
+                        <p>{brand.description}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
