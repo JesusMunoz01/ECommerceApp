4cc2dda81dcd9bdcedb8da9e3e1eafc0f3def460
Jesus <jesus.munoz01@utrgv.edu>
2024-05-19 21:19:00 -0500

    updated html for brand page

--- client/src/pages/brand.tsx
+++ client/src/pages/brand.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import ProductCard from "../components/Products/productCard";
 
 const BrandPage = () => {
     const { id } = useParams();
@@ -14,10 +15,22 @@ const BrandPage = () => {
 
     if(brandQuery.isLoading) return <div>Loading...</div>;
     if(brandQuery.isError) return <div>Error fetching brand</div>;
-    
+
     return (
         <div>
-            <h1>Brand</h1>
+            <div>
+                <h1>{brandQuery.data.data[0].name}</h1>
+                <p>{brandQuery.data.data[0].description}</p>
+                <img src={brandQuery.data.data[0].image} alt={brandQuery.data.data[0].name}/>
+            </div>
+            <div>
+                <h2>Products</h2>
+                <div className="grid grid-cols-5 gap-4 w-12/12 m-2">
+                    {brandQuery.data.data[0].products.map((product: any) => (
+                        <ProductCard key={product.id} product={product}/>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
