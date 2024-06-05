80eed721df507400416feebab296067d4befe8f5
Jesus <jesus.munoz01@utrgv.edu>
2024-06-04 21:14:08 -0500

    deleted brand component and updated format

--- client/src/pages/editBrand.tsx
+++ client/src/pages/editBrand.tsx
@@ -1,5 +1,4 @@
 import { useParams } from "react-router-dom";
-import EditBrand from "../components/Brands/editBrand";
 
 type EditBrandPageProps = {
     userData: { message: string; plan: string, brands: any[]}
@@ -15,7 +14,30 @@ const EditBrandPage = ({userData}: EditBrandPageProps) => {
     
     return (
         <div>
-            <EditBrand />
+            <h1>Edit Brand</h1>
+            <div>
+                <h2>Brand Details:</h2>
+                <div>
+                    <label>Name:</label>
+                    <input type="text" value={brand.name} />
+                </div>
+                <div>
+                    <label>Description:</label>
+                    <input type="text" value={brand.description} />
+                </div>
+                <div>
+                    <label>Image:</label>
+                    <input type="text" value={brand.image} />
+                </div>
+                <button>Save</button>
+            </div>
+            <div>
+                <h2>Products:</h2>
+                <div>
+                    Placeholder
+                </div>
+                <button>Add Product</button>
+            </div>
         </div>
     )
 };
