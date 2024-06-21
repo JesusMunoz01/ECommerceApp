db4e137b9964928ba6bdf60cf4dc9584844576d9
Jesus <jesus.munoz01@utrgv.edu>
2024-06-21 16:47:26 -0500

    updated tests for brands

--- client/cypress/e2e/brands/brand.cy.ts
+++ client/cypress/e2e/brands/brand.cy.ts
@@ -16,4 +16,20 @@ describe('Brand Tests', () => {
         cy.contains('Forest Workshop').click();
         cy.get('h1').should('have.text', 'Forest Workshop');
     });
+
+    it("should see the products of the brand", () => {
+        cy.contains('Forest Workshop').click();
+        cy.get('.grid').should('be.visible');
+        cy.contains('Wooden Desk').should('be.visible');
+    });
+
+    it("should add 2 quantities of the product to the cart", () => {
+        cy.contains('Forest Workshop').click();
+        cy.contains('Wooden Desk').click();
+        cy.get('.grid').contains("Wooden Desk").get('button').contains('+').click();
+        cy.get('.grid').contains("Wooden Desk").get('button').contains('Add to cart').click();
+        // Verify the cart
+        cy.contains('Cart').click();
+        // TODO: Check if the product is in the cart with the correct quantity
+    });
 });
