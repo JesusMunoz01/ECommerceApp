542d10f7b1ff1ec91112466d1cc7b72fdd9a23ea
Jesus <jesus.munoz01@utrgv.edu>
2024-06-23 20:40:10 -0500

    added form validation

--- client/src/components/Products/editUserForm.tsx
+++ client/src/components/Products/editUserForm.tsx
@@ -5,12 +5,15 @@ import { useState } from "react";
 const initialUser = {
     name: "",
     email: "",
-    password: ""
+    confirmEmail: "",
+    password: "",
+    confirmPassword: "",
 };
 
 const EditUserForm = () => {
     const { user } = useAuth0();
     const [updatedUser, setUpdatedUser] = useState(initialUser);
+    const [error, setError] = useState({ name: '', email: '', password: '' });
     const editUser = useMutation({
         mutationKey: ['editUser'],
         mutationFn: async () => {
@@ -38,8 +41,47 @@ const EditUserForm = () => {
         });
     };
 
+    const validateForm = () => {
+        let isValid = true;
+        let errors = { name: '', email: '', password: '' };
+
+        if (updatedUser.name !== "") {
+            if (updatedUser.name.length < 3 || updatedUser.name.length > 50) {
+                errors.name = 'Name must be between 5 and 50 characters';
+                isValid = false;
+            }
+        }
+        if (updatedUser.email !== "") {
+            if (updatedUser.email.length < 5 || updatedUser.email.length > 255) {
+                errors.email = 'Email must be between 5 and 255 characters';
+                isValid = false;
+            }
+            if(updatedUser.email !== updatedUser.confirmEmail) {
+                errors.email = 'Emails do not match';
+                isValid = false;
+            }
+        }
+        if (updatedUser.password !== "") {
+            if (updatedUser.password.length < 8 || updatedUser.password.length > 255) {
+                errors.password = 'Password must be between 8 and 255 characters';
+                isValid = false;
+            }
+            if(updatedUser.password !== updatedUser.confirmPassword) {
+                errors.password = 'Passwords do not match';
+                isValid = false;
+            }
+        }
+
+        setError(errors);
+
+        return isValid;
+    }
+
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
+        if (!validateForm()) {
+            return;
+        }
         editUser.mutate();
     };
 
@@ -47,12 +89,23 @@ const EditUserForm = () => {
         <div>
             <form onSubmit={handleSubmit} className="flex flex-col gap-2 bg-slate-600 p-4 border">
                 <h1>Edit User</h1>
-                <label htmlFor="name">Name</label>
-                <input type="text" placeholder="Name" name="name" value={updatedUser.name} onChange={handleChange} />
-                <label htmlFor="email">Email</label>
-                <input type="email" placeholder="Email" name="email" value={updatedUser.email} onChange={handleChange} />
+                <div className="flex flex-col gap-2 border-b-4 pb-4 border-slate-800">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" placeholder="Name" name="name" value={updatedUser.name} onChange={handleChange} />
+                    <span className="text-red-500">{error.name}</span>
+                </div>
+                <div className="flex flex-col gap-2 border-b-4 pb-4 border-slate-800">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" placeholder="Email" name="email" value={updatedUser.email} onChange={handleChange} />
+                    <label htmlFor="confirmEmail">Confirm Email</label>
+                    <input type="email" placeholder="Confirm Email" name="confirmEmail" value={updatedUser.confirmEmail} onChange={handleChange}/>
+                    <span className="text-red-500">{error.email}</span>
+                </div>
                 <label htmlFor="password">Password</label>
                 <input type="password" placeholder="Password" name="password" value={updatedUser.password} onChange={handleChange} />
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input type="password" placeholder="Confirm Password" name="confirmPassword"  value={updatedUser.confirmPassword} onChange={handleChange}/>
+                <span className="text-red-500">{error.password}</span>
                 <button type="submit" className="bg-gray-800 w-1/2 self-center mt-2">Update</button>
             </form>
         </div>
