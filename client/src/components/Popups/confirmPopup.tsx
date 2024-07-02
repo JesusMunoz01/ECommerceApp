7acbc941bb91d58449e8f5be6cbe9b32d6d682ec
Jesus <jesus.munoz01@utrgv.edu>
2024-07-01 19:05:35 -0500

    added buttons for each option

--- client/src/components/Popups/confirmPopup.tsx
+++ client/src/components/Popups/confirmPopup.tsx
@@ -8,7 +8,7 @@ type ConfirmationPopupProps = {
 const ConfirmationPopup = ({ title, message, onConfirm, onCancel }: ConfirmationPopupProps) => {
     return (
         <div className="w-full h-full flex flex-col justify-center items-center">
-            <div className="flex flex-col gap-4 bg-slate-700 w-5/12 h-2/5 justify-center items-center">
+            <div className="flex flex-col gap-4 bg-slate-700 w-5/12 h-2/5 justify-center items-center border-2 border-gray-900">
                 <h2 className="self-center text-3xl p-2">{title}</h2>
                 <p className="h-fit max-h-48">{message}</p>
                 <div className="self-center gap-2 flex pb-1 pt-1">
--- client/src/pages/settings.tsx
+++ client/src/pages/settings.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 import EditUserForm from '../components/Products/editUserForm';
 import ConfirmationPopup from '../components/Popups/confirmPopup';
+import Profile from '../components/Auth/auth0-profile';
 
 const SettingsPage = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [isSecurityOpen, setIsSecurityOpen] = useState(false);
+  const [isOrdersOpen, setIsOrdersOpen] = useState(false);
+  const [isEditOpen, setIsEditOpen] = useState(false);
   const [isDelete, setIsDelete] = useState(false);
 
   const handleToggle = (state: boolean, setState: (state: boolean) => void) => () => {
@@ -12,7 +16,7 @@ const SettingsPage = () => {
 
   return (
     <div className="flex w-full max-w-3xl mx-auto h-full flex-col items-center">
-      <h1 className='w-full text-center h-24'>Account Options</h1>
+      <h1 className='w-full text-center h-1/6'>Account Options</h1>
       {isDelete && 
         <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', zIndex: 1000 }}>
           <ConfirmationPopup 
@@ -23,24 +27,32 @@ const SettingsPage = () => {
           />
         </div>
       }
-      <div className="flex flex-col shadow-md rounded my-6 gap-4 h-full w-3/4">
-      <button 
-          className="block hover:bg-slate-400 focus:outline-non transition duration-150 ease-in-out w-full text-left px-4 py-2 rounded-none"
-          onClick={handleToggle(isOpen, setIsOpen)}>Peronal Information</button>
+      <div className="flex flex-col my-6 gap-4 h-4/6 w-3/4">
+        <button 
+            className="block hover:bg-slate-400 focus:outline-non transition duration-150 ease-in-out w-full text-left px-4 py-2 rounded-none"
+            onClick={handleToggle(isProfileOpen, setIsProfileOpen)}>Peronal Information
+        </button>
+        {isProfileOpen && (
+            <div className="border-t">
+                <Profile/>
+            </div>
+        )}
         <button 
           className="block hover:bg-slate-400 focus:outline-non transition duration-150 ease-in-out w-full text-left px-4 py-2 rounded-none"
-          onClick={handleToggle(isOpen, setIsOpen)}
+          onClick={handleToggle(isEditOpen, setIsEditOpen)}
         >
           Account Security
         </button>
-        {isOpen && (
+        {isEditOpen && (
             <div className="border-t">
                 <EditUserForm/>
             </div>
         )}
         <button 
           className="block hover:bg-slate-400 focus:outline-non transition duration-150 ease-in-out w-full text-left px-4 py-2 rounded-none"
-          onClick={handleToggle(isOpen, setIsOpen)}>View Orders</button>
+          onClick={handleToggle(isOrdersOpen, setIsOrdersOpen)}>View Orders</button>
+      </div>
+      <div className='flex flex-col shadow-md rounded my-6 gap-4 h-1/6 w-3/4'>
         <button 
           className="block hover:bg-red-500 bg-red-800 focus:outline-non transition duration-150 ease-in-out w-full text-left px-4 py-2 rounded-none"
           onClick={handleToggle(isDelete, setIsDelete)}
