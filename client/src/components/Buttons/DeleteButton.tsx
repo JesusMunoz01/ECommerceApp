53e11609fc7d30214b3a2c25b9b1e353cbc8bdf3
Jesus <jesus.munoz01@utrgv.edu>
2024-07-12 20:19:52 -0500

    added cancel subscription button and confirmation popup

--- client/src/components/Buttons/DeleteButton.tsx
+++ client/src/components/Buttons/DeleteButton.tsx
@@ -7,7 +7,7 @@ interface DeleteButtonProps {
 const DeleteButton = ({action, children, className}: DeleteButtonProps) => {
 
     return (
-        <button className={`block hover:bg-red-500 bg-red-800 focus:outline-non transition duration-150 ease-in-out text-left px-4 py-2 rounded-none ${className}`} onClick={action}>
+        <button className={`block hover:bg-red-500 bg-red-800 focus:outline-non transition duration-150 ease-in-out px-4 py-2 ${className}`} onClick={action}>
           {children ? children : "Delete User"}
         </button>
     );
