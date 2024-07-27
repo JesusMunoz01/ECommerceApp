107dd52a9e83e9af611514d04f4c5ce99d3eea34
Jesus <jesus.munoz01@utrgv.edu>
2024-07-26 20:36:27 -0500

    added createBrand query for frontend

--- client/src/components/Brands/createBrandForm.tsx
+++ client/src/components/Brands/createBrandForm.tsx
@@ -3,12 +3,39 @@ import { useUserProductsQuery } from "../../utils/useQueries";
 import Button from "../Buttons/Button";
 import { useState } from "react";
 import ProductSelectionPopup from "../Popups/productSelect";
+import { useMutation } from "@tanstack/react-query";
+
+type BrandData = {
+    name: string;
+    description: string;
+    image: string;
+    products: string[];
+}
 
 const CreateBrandForm = () => {
     const { user } = useAuth0();
     const [isPopupVisible, setIsPopupVisible] = useState(false);
     const [selectedProducts, setSelectedProducts] = useState<number[]>([]);
     const userProducts = useUserProductsQuery(user?.sub?.split('|')[1]);
+
+    const createBrandQuery = useMutation({
+        mutationKey: ['createBrand'],
+        mutationFn: async (brand: BrandData) => {
+            const response = await fetch('/api/brands', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(brand)
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to create brand');
+            }
+
+            return response.json();
+        }
+    })
 
     const handleProductSelection = (products: number[]) => {
         setSelectedProducts((prev) => [...prev, ...products]);
@@ -20,11 +47,11 @@ const CreateBrandForm = () => {
 
     return (
             <form className="flex flex-col justify-center items-center w-full max-w-2xl gap-2 border-slate-500 border p-2">
-                            {isPopupVisible && 
+                {isPopupVisible && 
                 <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', zIndex: 1000 }}>
                     <ProductSelectionPopup products={userProducts.data.products} onClose={togglePopup} onProductsAction={handleProductSelection} />
                 </div>
-            }
+                }
                 <div className="mb-3 flex flex-col w-full max-w-2xl">
                     <label htmlFor="brandName">Brand Name</label>
                     <input type="text"/>
