9fed511971bb98d80455a1553fe7ca01993b0b93
Jesus <jesus.munoz01@utrgv.edu>
2024-09-10 18:06:26 -0500

    updated order dtos

--- server/src/orders/dto/order.dto.ts
+++ server/src/orders/dto/order.dto.ts
@@ -1,7 +1,14 @@
 export class OrderDto {
-    readonly id: string;
-    readonly productsID: string[];
-    readonly ownerID: string;
-    readonly price: number;
+    userID: string;
+    total: number;
     status: "pending" | "completed" | "cancelled";
+    paymentMethod: string;
+    shippingAddress: string;
+}
+
+export class OrderItemDto {
+    orderId: number;
+    productId: number;
+    quantity: number;
+    price: number;
 }
