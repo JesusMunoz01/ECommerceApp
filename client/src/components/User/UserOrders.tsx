83272b3d371873f6a2ee301a70351e65dbeec800
Jesus <jesus.munoz01@utrgv.edu>
2024-09-19 21:59:18 -0500

    orders are now fetched successfully

--- client/src/components/User/UserOrders.tsx
+++ client/src/components/User/UserOrders.tsx
@@ -3,11 +3,13 @@ import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 type Orders = {
+    id: number
     userId: string;
     total: number;
     status: "Pending" | "Completed" | "Cancelled";
     paymentMethod: string;
     shippingAddress: string;
+    createdAt: Date;
     orderId: number;
     productId: number;
     quantity: number;
@@ -15,7 +17,6 @@ type Orders = {
 };
 
 const UserOrders = () => {
-    const [orders, setOrders] = useState<Orders[]>([]);
     const {user, getAccessTokenSilently} = useAuth0();
 
     const ordersQuery = useQuery({
@@ -27,36 +28,35 @@ const UserOrders = () => {
                     Authorization: `Bearer ${token}`,
                 },
             });
-            console.log(await response.json())
             return await response.json();
         },
     });
 
-    useEffect(() => {
-        setOrders(ordersQuery.data as Orders[]);
-    }, []);
+    if(ordersQuery.isLoading)
+        return <h1>Loading...</h1>
     
     return (
         <div className="flex flex-col gap-1 h-full">
         <h1 className="mb-2 min-h-20 text-3xl md:text-6xl">Your Orders</h1>
         <div>
         {/* TODO: Modify to user ordersQuery.data */}
-        {/* {orders.map((order) => (
+        {/* TODO: Format orders so that it has one order id with all the items */}
+        {ordersQuery.data.orders.map((order: Orders) => (
             <div key={order.id} className="border-t flex flex-col gap-2 mb-2">
             <h2 className="mt-2">Order ID: {order.id}</h2>
             <p>Total: ${order.total}</p>
             <p>Order Date: {new Date(order.createdAt).toLocaleString()}</p>
             <p>Order Status: {order.status}</p>
             <h3>Items:</h3>
-            <ul>
+            {/* <ul>
                 {order.items.map((item) => (
                 <li key={item.id}>
                     {item.name} - ${item.price}
                 </li>
                 ))}
-            </ul>
+            </ul> */}
             </div>
-        ))} */}
+        ))}
         </div>
         </div>
     );
