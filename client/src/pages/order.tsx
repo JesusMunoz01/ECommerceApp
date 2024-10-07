a1b4967a801aa464e1833dac6af8c25a13a0a505
Jesus <jesus.munoz01@utrgv.edu>
2024-10-06 19:46:39 -0500

    added query for order

--- client/src/pages/order.tsx
+++ client/src/pages/order.tsx
@@ -1,7 +1,29 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+
 const OrderPage = () => {
+    const { id } = useParams()
+    const { getAccessTokenSilently } = useAuth0()
+
+    const orderQuery = useQuery({
+        queryKey: ["order", id],
+        queryFn: async () => {
+            const token = getAccessTokenSilently()
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/order/${id}`, {
+                headers: {
+                  Authorization: `Bearer ${token}`
+                }
+              });
+              if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+              return response.json()
+        }
+    })
 
     const tempData = {
-        id: 12,
+        id,
         date: Date.now(),
         items: [{id:1, name: "item"}, {id:2, name: "item"}]
     }
