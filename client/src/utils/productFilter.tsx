14dcc8b113bbab4651b71bc076bd2a19149b58e4
Jesus <jesus.munoz01@utrgv.edu>
2024-10-24 18:39:36 -0500

    server and client/src/utils ts updated

--- client/src/utils/productFilter.tsx
+++ client/src/utils/productFilter.tsx
@@ -1,7 +1,8 @@
+import { Product } from "../components/Products/productCard";
 import { Orders } from "../components/User/UserOrders";
 
-export const productFilter = (data: any, search: string) => {
-    return data.filter((product: any) => product.name.toLowerCase().includes(search.toLowerCase()));
+export const productFilter = (data: Product[], search: string) => {
+    return data.filter((product: Product) => product.name.toLowerCase().includes(search.toLowerCase()));
 }
 
 export const orderFilter = (data: Orders[], search: string) => {
