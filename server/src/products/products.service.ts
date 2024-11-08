44b46313d8c802b23aef422c8de7daa10c4d8ccf
Jesus <jesus.munoz01@utrgv.edu>
2024-11-07 23:45:10 -0600

    note to add

--- server/src/products/products.service.ts
+++ server/src/products/products.service.ts
@@ -203,6 +203,7 @@ export class ProductsService {
 
     async createReview(userId: string, itemID: string, reviewData: ReviewDto): Promise<{ message: string; }> {
         try{
+            // Search to see if there is already a review
             await this.connection.query(`INSERT INTO reviews (productID, userID, review, rating, created_at, updated_at) VALUES 
             (?, ?, ?, ?, NOW(), NOW())`, [itemID, userId.split("|")[1], reviewData.review, reviewData.rating], (err, results) => {
                 if(err) {
